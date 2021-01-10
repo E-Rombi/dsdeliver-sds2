608c6098e1b79a04d61917142c0d9471dd10e76d
Eduardo Rombi <eduardo.rombi99@gmail.com>
2021-01-10 18:19:17 -0300

    Implementado ListProducts

---
+++ front-mobile/src/Routes.tsx
@@ -0,0 +1,30 @@
+import { NavigationContainer, Route } from "@react-navigation/native";
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from "./Home";
+import Orders from "./Orders";
+import React from 'react';
+
+const Stack = createStackNavigator();
+
+function Routes() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator 
+                headerMode="none"
+                screenOptions={{
+                    cardStyle: {
+                        backgroundColor: '#FFF'
+                    }
+                }}>
+
+                <Stack.Screen name="Home" component={Home}></Stack.Screen>
+                <Stack.Screen name="Orders" component={Orders}></Stack.Screen>
+
+            </Stack.Navigator>
+
+        </NavigationContainer>
+    );
+
+}
+
+export default Routes;
